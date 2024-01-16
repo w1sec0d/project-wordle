fef29cb1c2e0dafd6c76c66fb11e5830e1876f57
Carlos Ramírez <cadavid4003@gmail.com>
2024-01-16 10:30:10 -0500

    Add submit button and icons

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { ArrowRight } from "react-feather";
 
-function GuessInput({ handleSubmitGuess, disabled }) {
+function GuessInput({ handleSubmitGuess }) {
   const [guess, setGuess] = useState("");
 
   function handleSubmit(event) {
@@ -12,21 +13,25 @@ function GuessInput({ handleSubmitGuess, disabled }) {
   return (
     <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input
-        id="guess-input"
-        type="text"
-        value={guess.toUpperCase()}
-        onChange={(event) => {
-          setGuess(event.target.value.toUpperCase());
-        }}
-        pattern="[a-zA-Z]{5}"
-        title="5 letter word"
-        minLength={5}
-        maxLength={5}
-        required={true}
-        autoFocus={true}
-        disabled={disabled}
-      />
+      <div className="input-row">
+        <input
+          id="guess-input"
+          type="text"
+          value={guess.toUpperCase()}
+          onChange={(event) => {
+            setGuess(event.target.value.toUpperCase());
+          }}
+          pattern="[a-zA-Z]{5}"
+          title="5 letter word"
+          minLength={5}
+          maxLength={5}
+          required={true}
+          autoFocus={true}
+        />
+        <button type="submit" className="guess-input-icon">
+          <ArrowRight size={50} />
+        </button>
+      </div>
     </form>
   );
 }
